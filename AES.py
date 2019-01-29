4c9be37153e98a2b01802c7bfa334633411ba633
Rahul Ramakrishnan <40205748+LuciferShadox@users.noreply.github.com>
2019-01-29 20:29:32 +0530

    Update AES.py

--- AES.py
+++ AES.py
@@ -10,12 +10,13 @@ from sklearn.linear_model import LinearRegression
 from sklearn.externals import joblib
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.metrics import mean_squared_error
+import time
 
-
-def get_data(filename):
-    dataset=pd.read_excel(filename,parse_cols=6)
+def get_data():
+    dataset=pd.read_excel('training_set_rel3.xls',parse_cols=6)
     return dataset
 
+#bag of Words
 def BOW(essay):
     vectorizer = CountVectorizer(max_features = 5000, ngram_range=(1, 3), stop_words='english')
     countvectors=(vectorizer.fit_transform(essay)).toarray()
@@ -23,18 +24,43 @@ def BOW(essay):
     # featurenames=vectorizer.get_feature_names()
     return countvectors
 
-dataframe=get_data('training_set_rel3.xls')
-essay_set=dataframe['essay']
+#Parts of Speech (POS) count
+def POS(essay):
+    cleaned_essay=re.sub(r'\W', ' ', essay)
+    words=nltk.word_tokenize(cleaned_essay)
+    word_count=len(words)
+    sentences=nltk.sent_tokenize(essay)
+    sentences_count=len(sentences)
+    avg_len_sent=0
+    for sent in sentences:
+        avg_len_sent+=len(sent)
+    avg=avg_len_sent/sentences_count
+    return word_count,sentences_count,avg_len_sent
+
+
+start_time = time.time()
+dataframe=get_data()
+essay_set=dataframe[['essay']].copy()
 score=dataframe['domain1_score']
 #for testing purpose I decreased set
-essay_set=essay_set[:800]
-score=score[:800]
-countvectors=BOW(essay_set)
-x_train,x_test,y_train,y_test=train_test_split(countvectors,score,random_state=0, test_size=0.20)
+essay_set=essay_set[:1000]
+score=score[:1000]
+countvectors=BOW(essay_set['essay'])
+essay_set['word_count'],essay_set['sent_count'],essay_set['avg_sent_count']=zip(*essay_set['essay'].apply(POS))
+x=np.concatenate((essay_set.iloc[:,1:].values,countvectors),axis=1)
+
+x_train,x_test,y_train,y_test=train_test_split(x,score,random_state=0, test_size=0.20)
+
+
 model=LinearRegression()
 model.fit(x_train,y_train)
-#to save the model for future use
-joblib.dump(model,'mod.pkl')
+
 y_pred=model.predict(x_test)
+#check mean square error
 mse=mean_squared_error(y_test,y_pred)
 print ("Mean Squared Error :",mse)
+#Execution Time
+print("--- %s seconds ---" % (time.time() - start_time))
+
+#to save the model for future use
+joblib.dump(model,'mod.pkl')
